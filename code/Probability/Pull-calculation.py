e9c74aaa62ed62c11e8e706390f5558d42157c95
Playmakermz🍉 <60807663+playmakermz@users.noreply.github.com>
2025-08-20 05:33:27 +0700

    Update Pull-calculation.py

--- code/Probability/Pull-calculation.py
+++ code/Probability/Pull-calculation.py
@@ -1,5 +1,7 @@
 import random
 import time
+import statistics
+import os
 """
 # Teori dibalik ini 
 
@@ -21,11 +23,12 @@ Aturan penggunaan:
 2. lakukan pull 10 secara manual dan berjalan ke nilai mendekati yang tertinggi
 3. jika sudah dekat, semisal nilai tertinggi adalah 700, maka Real world pull harus mendekati 700.
 
-
+-------------------------------------------------------------------------
 > Laporan pull Ak (2% atau 0.0020):
-tertinggi adalah 362
+tertinggi adalah : 3422
 
 
+-------------------------------------------------------------------------
 Ini adalah Testing Ground untuk HSR. dimana probabilitas milik mereka adalah 0.6%.
 
 0.006 adalah angka desimal.
@@ -33,7 +36,7 @@ kita bisa ketahui bentuk persen dengan cara
 0.006 x 100
 
 > Laporan pull HSR (0.6% atau 0.0006):
-Streak tertinggi adalah 12423, 10191
+Streak tertinggi adalah 15520, 10191
 Modus Jackpot adalah 3986
 
 Record Pull HSR 19/08/2025:
@@ -45,7 +48,8 @@ Total jackpot: 112
 """
 
 # probabilitas jackpot
-p = 0.0006
+#p = 0.0006
+p = 0.0020
 
 # variabel statistik global
 total_pulls = 0
@@ -55,7 +59,16 @@ total_jackpot_terakhir = 0
 jackpot_list = []
 
 # Variabel sampai mendekati jackpot
-nilai_N = 12423
+nilai_N =  2589 #15520
+
+def clear_screen():
+    # For Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # For macOS and Linux
+    else:
+        _ = os.system('clear')
+
 
 def pull_once():
     """Satu kali pull"""
@@ -79,9 +92,9 @@ def pull_20():
     jarak_jackpot += 1
     if random.random() < p:
         total_jackpot += 1
-        print("=====================>  jackpot  <====================\n")
-        print(f"Jarak jackpot ke titik sekarang : {jarak_jackpot}")
-        print(f"Total pull jackkpot terakhir  : {total_jackpot_terakhir}")
+        #print("=====================>  jackpot  <====================\n")
+        #print(f"Jarak jackpot ke titik sekarang : {jarak_jackpot}")
+        #print(f"Total pull jackkpot terakhir  : {total_jackpot_terakhir}")
         jackpot_list.append(jarak_jackpot)
         total_jackpot_terakhir = jarak_jackpot
         jarak_jackpot = 0
@@ -94,19 +107,36 @@ def pull_ten():
         pull_once()
 
 
+def pull_auto():
+    """Satu kali pull"""
+    global total_pulls, total_jackpot, jarak_jackpot, total_jackpot_terakhir
+    total_pulls += 1
+    jarak_jackpot += 1
+    if random.random() < p:
+        total_jackpot += 1
+        print(f"====>  jackpot || Total Jackpot: {total_jackpot}  <====")
+        total_jackpot_terakhir = jarak_jackpot
+        jarak_jackpot = 0
+    else:
+        print(f"kamu tidak beruntung: {jarak_jackpot}")
+
+
 def jackpot_20():
     """Dapatkan 15 jackpot"""
     while total_jackpot <= 20:
         pull_20()
     jackpot_list.sort(reverse=True)
     print(jackpot_list)
+    modus = statistics.mode(jackpot_list)
+    print(f"Modus dari data adalah: {modus}")
 
 
 def automatic_pull():
     """Lakukkan pull otomatis setiap detik, hingga mendekati jackpot."""
     while jarak_jackpot < nilai_N:  # Ubah angka ini sesuai dengan jarak jackpot tertinggi yang didapatkan
-        time.sleep(0.003)
-        pull_once()
+        time.sleep(0.0001)
+        pull_auto()
+        clear_screen()
         if jarak_jackpot == (nilai_N - 10):  # Ubah angka ini sesuai dengan jarak jackpot tertinggi yang didapatkan
             print("====================> Real Word Pull Now! <=================")
             print(f"Total pull: {total_pulls}")
